f53ec07c4af08d09ec8b6b2261f3e598a940d09e
maxnatalia <Nataliamazur1988@gmail.com>
2023-12-07 11:31:27 +0100

    updated - added more styles for body

--- src/core/GlobalStyle.ts
+++ src/core/GlobalStyle.ts
@@ -10,6 +10,13 @@ html {
 };
 
 body {
+  color: ${({ theme }) => theme.color.text};
+  font-size: 18px;
+  font-weight: 400;
+  letter-spacing: 0.9px;
+  line-height: 140%;
+  font-family: 'Inter', sans-serif;
   background: ${({ theme }) => theme.color.background};
+  transition: all 0.3s ease;
 }
 `;
