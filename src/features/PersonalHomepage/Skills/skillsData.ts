645e44a49b6d8a44f7220b28dc4cc06a19b20ca9
maxnatalia <Nataliamazur1988@gmail.com>
2023-12-10 09:27:00 +0100

    added skillsData for Skills component

---
+++ src/features/PersonalHomepage/Skills/skillsData.ts
@@ -0,0 +1,53 @@
+import tools from "./tools.png";
+import rocket from "./rocket.png";
+
+export const skillsData = [
+    {
+        title: "My skillset includes",
+        icon: tools,
+        list: [
+            "Semantic & accessible HTML",
+            "CSS BEM convention",
+            "CSS Flexbox",
+            "CSS Grid",
+            "CSS animations",
+            "Framer Motion",
+            "Responsive Web Design",
+            "SASS",
+            "Markdown",
+            "JavaScript (ES6+)",
+            "Immutability",
+            "TypeScript",
+            "Promises, Async/Await",
+            "React",
+            "React Hooks",
+            "React Router",
+            "React Query",
+            "Styled-Components",
+            "Material UI",
+            "Tailwind",
+            "Redux (Toolkit)",
+            "Redux-Saga",
+            "Working with API (fetch, axios)",
+            "Error handling",
+            "NPM",
+            "Vite",
+            "Git",
+            "GitHub Pull Requests & Reviews",
+            "Teamwork",
+            "Scrum",
+            "Trello"
+        ],
+    },
+    {
+        title: "What I want to learn next",
+        icon: rocket,
+        list: [
+            "Further develop acquired skills",
+            "Software testing",
+            "Next.js",
+            "Node.js and Express",
+            "MongoDB and MySQL databases",
+        ],
+    },
+];
