96406e8fcc39b78481538fc00807237aa58912b4
maxnatalia <Nataliamazur1988@gmail.com>
2023-12-11 11:32:27 +0100

    created reusable Button component style

---
+++ src/features/PersonalHomepage/Button.ts
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  padding: 12px 16px;
+  display: inline-flex;
+  align-items: center;
+  gap: 16px;
+  color: ${({ theme }) => theme.color.buttonText};
+  text-decoration: none;
+  text-transform: capitalize;
+  font-size: 20px;
+  font-weight: 600;
+  letter-spacing: 1px;
+  background-color: ${({ theme }) => theme.color.buttonBg};
+  border: 1px solid ${({ theme }) => theme.color.buttonBorder};
+  border-radius: 4px;
+  cursor: pointer;
+  transition: 0.3s ease-in-out;
+
+  &:hover {
+    box-shadow: ${({ theme }) => theme.color.buttonHover};
+  }
+
+  &:active {
+    box-shadow: ${({ theme }) => theme.color.buttonActive};
+  }
+`;
