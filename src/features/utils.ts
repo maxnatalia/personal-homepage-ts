c8b56100431ba79c433f48cc71870602561f37c5
maxnatalia <Nataliamazur1988@gmail.com>
2023-12-13 09:51:20 +0100

    added utils file with basic data

---
+++ src/features/utils.ts
@@ -0,0 +1,8 @@
+export const userName = "maxnatalia";
+export const baseUrl = "https://api.github.com";
+export const apiLink = `${baseUrl}/users/${userName}/repos`;
+
+export const email = "nataliamazur1988@gmail.com";
+export const gitHubLink = `https://github.com/${userName}`;
+export const linkedInLink =
+  "https://www.linkedin.com/in/natalia-mazur-%C5%BCurek-136a35254/";
