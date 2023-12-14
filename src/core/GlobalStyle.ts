c62cec4d4ca60317e19c78f7116a4138b7cfc392
maxnatalia <Nataliamazur1988@gmail.com>
2023-12-14 11:53:44 +0100

    improved - changed theme name and added word-break

--- src/core/GlobalStyle.ts
+++ src/core/GlobalStyle.ts
@@ -10,12 +10,13 @@ html {
 };
 
 body {
-  color: ${({ theme }) => theme.color.text};
+  color: ${({ theme }) => theme.color.textPrimary};
   font-size: 18px;
   font-weight: 400;
   letter-spacing: 0.9px;
   line-height: 140%;
   font-family: 'Inter', sans-serif;
+  word-break: break-word;
   background: ${({ theme }) => theme.color.background};
   transition: all 0.3s ease;
 }
