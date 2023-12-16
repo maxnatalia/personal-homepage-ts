20ac4f6052babe1a2519d86ca9a6cd10e7665501
maxnatalia <Nataliamazur1988@gmail.com>
2023-12-16 10:09:15 +0100

    added ImageIcon, added media queries

--- src/features/PersonalHomepage/Skills/styled.ts
+++ src/features/PersonalHomepage/Skills/styled.ts
@@ -4,7 +4,27 @@ export const Section = styled.section`
   margin: 72px 0;
   padding: 32px;
   background: ${({ theme }) => theme.color.sectionBackground};
-  box-shadow: ${({ theme }) => theme.shadows.primary};
+  box-shadow: ${({ theme }) => theme.color.tileShadow};
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.tablet}px) {
+    margin: 72px 16px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    margin: 48px 16px;
+    padding: 16px;
+  }
+`;
+
+export const ImageIcon = styled.img`
+  width: 26px;
+  height: 32px;
+  object-fit: cover;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    width: 17px;
+    height: 21px;
+  }
 `;
 
 export const SkillsList = styled.ul`
@@ -14,6 +34,14 @@ export const SkillsList = styled.ul`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   gap: 8px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.tablet}px) {
+    grid-template-columns: 1fr 1fr;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    grid-template-columns: 1fr;
+  }
 `;
 
 export const SkillItem = styled.li`
@@ -21,6 +49,11 @@ export const SkillItem = styled.li`
   align-items: center;
   gap: 8px;
   color: ${({ theme }) => theme.color.textSecondary};
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    font-size: 14px;
+    letter-spacing: 0.7px;
+  }
 `;
 
 export const Bullet = styled.div`
@@ -28,4 +61,9 @@ export const Bullet = styled.div`
   height: 9px;
   border-radius: 50%;
   background: ${({ theme }) => theme.color.bullet};
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    width: 6px;
+    height: 6px;
+  }
 `;
