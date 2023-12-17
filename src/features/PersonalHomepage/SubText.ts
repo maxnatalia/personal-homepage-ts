c5162f7d8263798d373393f0d36003d441825219
maxnatalia <Nataliamazur1988@gmail.com>
2023-12-17 08:30:41 +0100

    updated - added media queries for SubText

--- src/features/PersonalHomepage/SubText.ts
+++ src/features/PersonalHomepage/SubText.ts
@@ -3,4 +3,8 @@ import styled from "styled-components";
 export const SubText = styled.span`
   font-size: 20px;
   letter-spacing: 1px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    font-size: 17px;
+  }
 `;
