c9ec8d9523b9b9a3e4e5c3fa19284de4f4c24593
maxnatalia <Nataliamazur1988@gmail.com>
2023-12-19 08:50:33 +0100

    updated - added media queries and icons styling

--- src/features/PersonalHomepage/Footer/styled.ts
+++ src/features/PersonalHomepage/Footer/styled.ts
@@ -1,19 +1,27 @@
 import styled, { css } from "styled-components";
+import GitHubLogo from "./gitHub.svg?react";
+import LinkedInLogo from "./linkedIn.svg?react";
 
 type StyledLinkProps = {
   $email?: boolean;
 };
 
 export const StyledFooter = styled.footer`
+  padding: 0 16px;
   margin-bottom: 108px;
   max-width: 670px;
   display: flex;
   flex-direction: column;
   gap: 24px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    margin-bottom: 30px;
+    gap: 12px;
+  }
 `;
 
 export const StyledLink = styled.a<StyledLinkProps>`
-  color: ${({ theme }) => theme.color.text};
+  color: ${({ theme }) => theme.color.textPrimary};
   cursor: pointer;
   transition: color 0.3s ease-in;
 
@@ -23,6 +31,10 @@ export const StyledLink = styled.a<StyledLinkProps>`
       text-decoration: none;
       font-weight: 900;
       font-size: 32px;
+
+      @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+        font-size: 17px;
+      }
     `}
 
   &:hover {
@@ -35,4 +47,27 @@ export const LogoBox = styled.div`
   display: flex;
   align-items: center;
   gap: 24px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    margin-top: 28px;
+    gap: 16px;
+  }
+`;
+
+const iconStyles = css`
+  width: 48px;
+  height: 48px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    width: 32px;
+    height: 32px;
+  }
+`;
+
+export const GithubIcon = styled(GitHubLogo)`
+  ${iconStyles}
+`;
+
+export const LinkedInIcon = styled(LinkedInLogo)`
+  ${iconStyles}
 `;
