46b436a35d33b308d11d582ef264d46650acc513
maxnatalia <Nataliamazur1988@gmail.com>
2023-12-19 09:11:31 +0100

    updated - added media queries

--- src/features/PersonalHomepage/Header/styled.ts
+++ src/features/PersonalHomepage/Header/styled.ts
@@ -1,18 +1,43 @@
 import styled from "styled-components";
+import Envelope from "./message.svg?react";
 
 export const StyledHeader = styled.header`
   margin-top: 116px;
   display: flex;
-  gap: 66px;
   align-items: center;
+  gap: 66px;
   position: relative;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.tablet}px) {
+    padding: 0 16px;
+    margin-top: 76px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    margin-top: 32px;
+    flex-direction: column;
+    align-items: start;
+    gap: 12px;
+  }
 `;
 
 export const Image = styled.img`
+  padding-left: 16px;
   width: 398px;
   height: 398px;
   object-fit: cover;
   border-radius: 50%;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.tablet}px) {
+    padding-left: 0;
+    width: 240px;
+    height: 240px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    width: 132px;
+    height: 132px;
+  }
 `;
 
 export const Name = styled.h1`
@@ -22,6 +47,14 @@ export const Name = styled.h1`
   font-weight: 900;
   line-height: 100%;
   letter-spacing: 1.9px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    margin-top: 8px;
+    margin-bottom: 16px;
+    font-size: 22px;
+    line-height: 100%;
+    letter-spacing: 1.1px;
+  }
 `;
 
 export const Description = styled.p`
@@ -30,10 +63,25 @@ export const Description = styled.p`
   font-size: 20px;
   font-weight: 400;
   letter-spacing: 1px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    font-size: 17px;
+    letter-spacing: 0.85px;
+  }
+`;
+
+export const EnvelopeIcon = styled(Envelope)`
+  width: 24px;
+  height: 24px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    width: 20px;
+    height: 20px;
+  }
 `;
 
 export const ThemeBox = styled.div`
   position: absolute;
   top: -12px;
-  right: 0;
+  right: 16px;
 `;
