75fba456688c387c5dabd96e0b681457a775f509
maxnatalia <Nataliamazur1988@gmail.com>
2023-12-19 09:09:16 +0100

    improved imports for useFetchRepos

--- src/features/PersonalHomepage/Portfolio/index.tsx
+++ src/features/PersonalHomepage/Portfolio/index.tsx
@@ -1,4 +1,4 @@
-import { useFetchRepos } from "../../useFetchRepos";
+import { useFetchRepos } from "./useFetchRepos";
 import { SubText } from "../SubText";
 import { Title } from "../Title";
 import Error from "./Error";
@@ -8,6 +8,7 @@ import { Icon, Section, Wrapper } from "./styled";
 
 const Portfolio = () => {
   const { data, isLoading, error } = useFetchRepos();
+
   return (
     <Section>
       <Wrapper>
@@ -15,14 +16,9 @@ const Portfolio = () => {
         <Title>Portfolio</Title>
         <SubText>My recent projects</SubText>
       </Wrapper>
-
       {isLoading && <Loading />}
       {error && <Error />}
-      {data && <Repos />}
-
-      {/* {error && <p>error</p>}
-      {isLoading && <p>Loading...</p>}
-      {data && data.map(el => <div key={el.id}>{el.name}</div>)} */}
+      {data && <Repos data={data} />}
     </Section>
   );
 };
